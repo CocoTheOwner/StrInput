75abd499ed3a8181431032f54b28a5cc3a41f2ca
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-11-29 16:32:50 +0100

    add package info details on spigotplugin

--- src/main/java/nl/codevs/strinput/examples/spigotmc/package-info.java
+++ src/main/java/nl/codevs/strinput/examples/spigotmc/package-info.java
@@ -2,6 +2,8 @@
  * Spigot plugin implementation using this commands system.<br>
  * Main classes to pay attention to:
  * <ul>
+ *     <li>{@link nl.codevs.strinput.examples.spigotmc.SpigotPlugin}
+ *     An example of an implemented plugin class with the command system.</li>
  *     <li>{@link nl.codevs.strinput.examples.spigotmc.SpigotCenter}
  *     The command center which is attached to the main class of a plugin.</li>
  *     <li>{@link nl.codevs.strinput.examples.spigotmc.SpigotUser}
