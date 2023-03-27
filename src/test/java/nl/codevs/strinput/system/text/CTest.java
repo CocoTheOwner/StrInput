1763ef3ad3a18b4db53cef89c4596f63df094366
Sjoerd van de Goor <sjoerdvdgoor@hotmail.com>
2023-03-27 21:58:36 +0200

    Color unit tests

--- src/test/java/nl/codevs/strinput/system/text/CTest.java
+++ src/test/java/nl/codevs/strinput/system/text/CTest.java
@@ -1,4 +1,15 @@
+package nl.codevs.strinput.system.text;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CTest {
-  
+
+    @Test
+    void removeColor() {
+        assertEquals("No Colors Here", C.removeColor(C.GREEN + "No" + C.RED + " Colors " + C.YELLOW + "Here"));
+        assertEquals("No Colors Here", C.removeColor(C.GREEN.toString() + C.RED + "No Colors " + C.YELLOW + "Here"));
+        assertEquals("No Colors Here", C.removeColor(C.GREEN + "No" + C.RED + " Colors " + C.COLOR_CHAR + " " + C.YELLOW + "Here"));
+    }
 }
